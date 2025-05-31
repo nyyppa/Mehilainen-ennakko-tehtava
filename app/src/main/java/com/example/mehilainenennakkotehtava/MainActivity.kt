403a52f833795a4ad57fb970b95649225bd4dc96
nyyppa <atte.yliverronen@gmail.com>
2025-05-31 21:21:43 +0300

    -added 2 second delay on loading the JSON file -added loading screen with spinner that spins during the loading time

--- app/src/main/java/com/example/mehilainenennakkotehtava/MainActivity.kt
+++ app/src/main/java/com/example/mehilainenennakkotehtava/MainActivity.kt
@@ -3,21 +3,50 @@ package com.example.mehilainenennakkotehtava
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import kotlinx.serialization.json.Json
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import kotlinx.coroutines.delay
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Replace with reading from assets or network if needed
-        val jsonString = assets.open("TestiJson.txt").bufferedReader().use { it.readText() }
-        val model = Json { ignoreUnknownKeys = true }.decodeFromString<UIModel>(jsonString)
-
         setContent {
             MaterialTheme {
-                DynamicUI(model, lang = "en")
+                val uiModelState = produceState<UIModel?>(initialValue = null) {
+                    delay(2000) // Simulate loading delay
+                    val jsonString = assets.open("TestiJson.txt").bufferedReader().use { it.readText() }
+                    val model = Json { ignoreUnknownKeys = true }.decodeFromString<UIModel>(jsonString)
+                    value = model
+                }
+
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    if (uiModelState.value == null) {
+                        LoadingScreen()
+                    } else {
+                        DynamicUI(uiModelState.value!!, lang = "en")
+                    }
+                }
             }
         }
+    }
+
+
+}
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
     }
 }
